106a857fe4c31cf39126c1e3d5859a53a70a5fde
TuDouuuuu <932087357@qq.com>
2019-05-24 14:27:34 +0800

    修复了读入数据后会马上对txt文件进行操作的BUG。

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,51 +6,49 @@
 #include"order.h"
 
 void menu(){
-	cout<<"		================================================="<<endl;
+	cout<<"		===================================================="<<endl;
 	cout<<"			Welcome to the order manage system!!"<<endl;
 	cout<<"				1.Add a new order"<<endl;
 	cout<<"				2.Change an order's information"<<endl;
 	cout<<"				3.display all orders"<<endl;
 	cout<<"				4.Find some orders"<<endl;
 	cout<<"				5.Delete an order"<<endl;
-	cout<<"		================================================="<<endl;
+	cout<<"				6.save and exit"<<endl;
+	cout<<"		===================================================="<<endl;
 }
 
 int main(){
 	cout<<"Loading..."<<endl;
 	ifstream inFile;
 	inFile.open("..\\\\data_example\\data.txt");
-	ofstream outFile;
-	outFile.open("..\\\\data_example\\data.txt");
+	
+	//构建链表的头（空的
 	static int cnt=0;
 	Order root(cnt);cnt++;
+
+	//读入已有数据
 	string s1,s2;int temp,p,n;
 	Order* u=&root;
 	while(inFile>>temp>>s1>>s2>>p>>n){
 		u=u->create(u,cnt,s1,s2,p,n);
 		cnt++;
 	}
-	/*int op;
+
+	//用于测试
+	/*root.search("-C","cyz",&root);
+	menu();*/
+
+	int op;
 	while(cin>>op){
-
+		menu();
 	}
-	*/
-	root.search("-D","ddsaf",&root);
-
-	cout<<"Welcome to the Order manage system!!"<<endl;
 
 
-/*
-	char *savePath = "..\\\\data_example\\test1.txt";
-    if(remove(savePath)==0)
-    {
-        cout<<"删除成功"<<endl;        
-    }
-    else
-    {
-        cout<<"删除失败"<<endl;        
-    }
-*/
+
+	//输出文件用：
+	//ofstream outFile;
+	//outFile.open("..\\\\data_example\\data.txt");
+
 
 	//ofstream cout("..\\\\data_example\\test1.txt");
 }
